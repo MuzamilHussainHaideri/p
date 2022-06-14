4d6a876c832b257e8bfeeadf4bc46ed0ca627158
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2022-06-14 11:24:47 +0500

    Pagination added + Bootstrap added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.order(create_at: :desc).page(params[:page])
   end
 
   # GET /users/1 or /users/1.json
