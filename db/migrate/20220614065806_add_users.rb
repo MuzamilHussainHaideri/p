c69577b164975043de32895bad740698ff568096
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2022-06-14 12:13:30 +0500

    error fixes

---
+++ db/migrate/20220614065806_add_users.rb
@@ -0,0 +1,10 @@
+class AddUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :father_name
+
+      t.timestamps
+    end
+  end
+end
